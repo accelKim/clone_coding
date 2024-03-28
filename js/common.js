6d388ee8167a9cac0f3466d48a1a2d1a210e2ea9
accelKim <dnjsgh567877@gmail.com>
2024-03-28 18:00:38 +0900

    after Responsive v1.0

--- js/common.js
+++ js/common.js
@@ -1,13 +1,27 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
+const faqToggles = document.querySelectorAll('.faq-toggle');
+
+faqToggles.forEach((toggle) => {
+    toggle.addEventListener('click', () => {
+        const faqListItem = toggle.closest('.faq-list');
+
+        faqListItem.classList.toggle('on');
+    });
+});
+
+const gnb = document.querySelector('.gnb');
+const ham = document.querySelector('.ham');
+ham.addEventListener('click', (e) => {
+    gnb.classList.toggle('on');
+});
+
+
+var swiper = new Swiper('.mySwiper', {
     spaceBetween: 30,
-    loop: true,
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+        el: '.swiper-pagination',
     },
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
-  });
+});
